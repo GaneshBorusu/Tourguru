c1d22513a89d662bc65d20aa89a980486cd51167
GaneshBorusu <ganeshborusu9@gmail.com>
2025-03-08 13:03:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Tourguru
-Tourguru Website
+# TourGuru
+This is a Tour agency website designed using Tailwand CSS , Html and Vanilla Javascript.
+
+# Home 
+
+<img width="960" alt="image" src="https://github.com/Git-Hemanth/TourGuru/assets/147172782/a79d34f1-d258-4e66-bd08-d590327a405a">
+
+
+# Contact Form
+Contact Form is powered with Smtp(Simple mail transfer protocol) Javascript Library Mailing System for the Client-side and server-side management
+</br>SmtpJs API:https://smtpjs.com
+
+<img width="957" alt="image" src="https://github.com/Git-Hemanth/TourGuru/assets/147172782/a0dea80d-d3a9-4b42-abdb-7bbaa63a31a8">
+
+
+
---
+++ script.js
@@ -0,0 +1,70 @@
+let navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.add('active');
+}
+
+document.querySelector('#close-search').onclick = () =>{
+    searchForm.classList.remove('active');
+}
+window.onscroll=()=>{
+    navbar.classList.remove('active');
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+window.onload=()=>{
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+const form=document.querySelector('form');
+const email=document.getElementById('email');
+const phone=document.getElementById('phone');
+const subject=document.getElementById('subject');
+const message=document.getElementById('message');
+const Fulname=document.getElementById('name');
+
+function sendEmail(){
+    const bodyMessage=`Full Name:${Fulname.value}<br>Email:${email.value}<br> 
+    Phone Number:${phone.value}<br>Subject:${subject.value}<br>Message:${message.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "hemanthhere12@gmail.com",
+        Password : "8A1DC0686382CCCADF818005DB9B69B18189",
+        To : 'hemanthhere12@gmail.com',
+        From : "hemanthhere12@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if(message=="OK"){
+            Swal.fire({
+                title: "Thank You For Choosing Us!",
+                text: "We will reach out you Soon!",
+                icon: "success"
+              });
+        }
+    });
+}
+
+    form.addEventListener("submit",(e) => {
+        e.preventDefault();
+    
+        sendEmail();
+    });
+    
